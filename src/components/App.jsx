c41ee31237f7d9d6375b1e84c366ab9cec0cde80
MikoreVikt <136119613+MrTkachenkoA@users.noreply.github.com>
2023-06-11 16:48:49 +0300

    adds start files

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import { Navbar } from "./Navbar";
+import { MainContent } from "./MainContent";
+import { Footer } from "./Footer";
+
+export const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+};
